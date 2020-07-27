1d023543182036071215defe6572e7258c8a61dd
du_wenlei <823633682@qq.com>
2020-07-27 18:08:11 +0800

    first commit

---
+++ src/main/java/com/hiooih/AbdpApplication.java
@@ -0,0 +1,13 @@
+package com.hiooih;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbdpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbdpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hiooih/AbdpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiooih;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbdpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
