040114f70415f800b6d48624d1eea74a84089176
du_wenlei <823633682@qq.com>
2020-07-30 18:06:52 +0800

    处理 layout 相关信息

--- src/main/java/com/hiooih/abdp/controller/IndexController.java
+++ src/main/java/com/hiooih/abdp/controller/IndexController.java
@@ -2,17 +2,15 @@ package com.hiooih.abdp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author duwenlei
  **/
 @Controller
-@RequestMapping("/index")
 public class IndexController {
 
-    @GetMapping("/index")
+    @GetMapping("/")
     public ModelAndView index() {
         return new ModelAndView("index");
     }
